86324e8e4e83f6d515f443a206d5d0ad0ecab6c7
WardSong <1556824234@qq.com>
2025-10-10 18:00:47 +0800

    链接超时控制

--- database/db_session.py
+++ database/db_session.py
@@ -37,7 +37,13 @@ def get_async_engine(db_type: str = None):
     else:
         raise ValueError(f"Unsupported database type: {db_type}")
 
-    engine = create_async_engine(db_url, echo=False, pool_pre_ping=True, pool_recycle=3600)
+    engine = create_async_engine(
+        db_url, echo=False, pool_pre_ping=True, pool_recycle=3600,
+        connect_args={
+        "connect_timeout": 120,  # 建立连接的超时时间
+        "read_timeout": 180,     # 读超时
+        # "write_timeout": 180     # 写超时
+    },)
     _engines[db_type] = engine
     return engine
 
