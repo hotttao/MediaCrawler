8466508564fe7dc160ac1d5d8da3a9aed9d5f959
tsong-mini <tsong-mini@qq.com>
2026-04-18 14:09:15 +0800

    更新抓取的账户

--- config/account_config.py
+++ config/account_config.py
@@ -1,14 +1,20 @@
 ACCOUNTS = [
-    {
-        "account_id": "account_1",
-        "nickname": "橙子Mama",
-        "phone": "19156537759",
-        "cookies": "",
-    },
+    # {
+    #     "account_id": "account_1",
+    #     "nickname": "橙子Mama",
+    #     "phone": "19156537759",
+    #     "cookies": "",
+    # },
     {
         "account_id": "account_2",
         "nickname": "漫游者",
         "phone": "13826124760",
+        "cookies": "",
+    },
+    {
+        "account_id": "42258334788",
+        "nickname": "去看看",
+        "phone": "19965230238",
         "cookies": "",
     },
 ]
